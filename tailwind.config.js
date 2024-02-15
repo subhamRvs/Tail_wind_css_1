1af8382edebb33183182ed829bd40d23c940d619
Subham <subham@rvsmedia.com>
2024-02-15 10:52:38 +0530

    first commit 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      backgroundImage: {
+        'banner': "url('/img/back-img.jpg')", // Update the URL to the correct path
+        // 'footer-texture': "url('../img/back-img.jpg')",
+      }
+    }
+  }
 }
-
